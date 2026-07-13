2c89fc9c32df603d840b06373fea51953402f78d
carlos123asd <carlos-medin@hotmail.com>
2026-07-13 17:47:50 +0200

    ADD: estructura del layout auth, left imagen, right formulario - (sin materials)

--- frontend/src/app/features/auth/auth-routing.module.ts
+++ frontend/src/app/features/auth/auth-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
